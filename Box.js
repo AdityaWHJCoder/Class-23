805295704a8aa572daa13ee511b8066cd230f7c0
AdityaWHJCoder <72691990+AdityaWHJCoder@users.noreply.github.com>
2020-10-25 17:49:37 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,31 @@
+class Box{
+    constructor(x, y, width, height, fill1){
+        var options = {
+            'restitution': 0.8, 
+            'friction': 0.3,
+            'density': 1.0
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.fill = fill1;
+
+        World.add(world,this.body);
+
+    }
+
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        // fill(255);
+        rect(0, 0, this.width, this.height, this.fill);
+        pop();
+
+    }
+}
